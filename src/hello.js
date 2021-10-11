835430c1760a1f23c0492660aa5c46ae42afe165
Lokisley Oliveira <lokisley@hotmail.com>
2021-10-11 01:44:12 -0300

    :recycle: Create React app

---
+++ src/hello.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const hello = <h1> This is just a test, boy. </h1>
+
+export default hello
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+
+import smile from './hello'
+
+ReactDOM.render(smile, document.getElementById('root'));
